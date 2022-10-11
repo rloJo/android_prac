346e4554e8d167371463a3ca25711f05363877c7
rloJo <114863642+rloJo@users.noreply.github.com>
2022-10-11 21:59:31 +0900

    Create MainActivity.kt

---
+++ Notification/MainActivity.kt
@@ -0,0 +1,87 @@
+package com.example.notification
+
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+
+
+class MainActivity : AppCompatActivity() {
+    var num = 0 // 알림 숫자 증가를 위한 변수
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener{
+            //알림을 시작
+            showNotification()
+        }
+
+        //권한요청
+        requestSinglePermission(Manifest.permission.POST_NOTIFICATIONS)
+        createNotificationChannel()
+    }
+
+    private val channelID = "default"
+
+    private fun showNotification() { //알림 시작하는 함수
+        num++
+        val builder = NotificationCompat.Builder(this, channelID)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setContentTitle("Notification Lab")
+            .setContentText("Notification #$num")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+        NotificationManagerCompat.from(this)
+            .notify(1, builder.build()) // 아이디가 계속 1이기 떄문에 기존알림에 덮어 씀
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel() { //알림을 생성하는 함수
+        val channel = NotificationChannel(
+            channelID, "default channel",
+            NotificationManager.IMPORTANCE_DEFAULT
+        )
+        channel.description = "description text of this channel."
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel) 
+    }
+
+    private fun requestSinglePermission(permission: String) { //권한을 요청하는 함수
+        if (checkSelfPermission(permission) == PackageManager.PERMISSION_GRANTED)
+            return
+
+        val requestPermLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+            if (it == false) { // permission is not granted!
+                AlertDialog.Builder(this).apply {
+                    setTitle("Warning")
+                    setMessage("warning")
+                }.show()
+            }
+        }
+
+        if (shouldShowRequestPermissionRationale(permission)) {
+            // you should explain the reason why this app needs the permission.
+            AlertDialog.Builder(this).apply {
+                setTitle("Reason")
+                setMessage("Reason")
+                setPositiveButton("Allow") { _, _ -> requestPermLauncher.launch(permission) }
+                setNegativeButton("Deny") { _, _ -> }
+            }.show()
+        } else {
+            // should be called in onCreate()
+            requestPermLauncher.launch(permission)
+        }
+    }
+
+}
