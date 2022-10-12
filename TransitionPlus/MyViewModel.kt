0660dbb2116139e19b04714f2895a088f1136415
rloJo <114863642+rloJo@users.noreply.github.com>
2022-10-12 21:14:01 +0900

    Create MyViewModel.kt

---
+++ TransitionPlus/MyViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.transition
+
+import androidx.lifecycle.ViewModel
+
+class MyViewModel(): ViewModel() {
+    var str1: String = "Name"
+    var str2: String = "Name"
+    var checked : Int = 0
+
+
+}
