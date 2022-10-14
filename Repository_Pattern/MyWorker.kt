f236834dc77077ea25d0aab7bb18439ec55f0af0
LeeEunWoo <114863642+rloJo@users.noreply.github.com>
2022-10-14 23:49:48 +0900

    Create MyWorker.kt

---
+++ Repository_Pattern/MyWorker.kt
@@ -0,0 +1,22 @@
+package com.example.repository_pattern
+
+import android.content.Context
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+
+class MyWorker(appContext: Context, params: WorkerParameters) : CoroutineWorker(appContext, params) {
+    override suspend fun doWork(): Result {
+        val username = inputData.getString("username") ?:""
+        val repository = MyRepository(applicationContext)
+        try {
+            repository.refreshData(username)
+        } catch (e: Exception) {
+            return Result.retry()
+        }
+        return Result.success()
+    }
+
+    companion object {
+        const val name = "com.example.repository_pattern.MyWorker"
+    }
+}
